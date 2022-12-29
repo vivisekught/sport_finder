453a1edb50b5894edd576a7dbffcdbe3240f9663
artisoul0 <yur4ikff12@gmail.com>
2022-12-29 19:13:27 +0200

    global state for kinds of sport

--- client/client_side/src/index.js
+++ client/client_side/src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import User from "./sport_finder_actually/User";
+import KindsOfSport from "./sport_finder_actually/KindsOfSport";
 
+export const Context = createContext(null)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Context.Provider value={{
+        user: new User(),
+        training: new KindsOfSport(),
+    }}>
+        <App />
+    </Context.Provider>
+
 );
 
