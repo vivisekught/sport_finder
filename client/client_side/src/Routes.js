bf540a3388e31f72d0d768da00b4210b433bd5b6
artisoul0 <yur4ikff12@gmail.com>
2022-12-29 18:24:23 +0200

    added some public routes

--- client/client_side/src/Routes.js
+++ client/client_side/src/Routes.js
@@ -1,11 +1,24 @@
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "./utils/consts";
+import Auth from "./pages/Auth";
+
 export const authRoutes = [
     {
-        path:
+
     }
 ]
 
 
 
-export const publicRoutes = []
+export const publicRoutes = [
+    {
+        path: REGISTRATION_ROUTE,
+        Component: Auth
+    },
+    {
+        path : LOGIN_ROUTE,
+        Component: Auth,
+    },
+
+]
 
 
