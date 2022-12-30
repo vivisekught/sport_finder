0b519739b2e24fdf16b83c76d5c54e48450e26e3
artisoul0 <yur4ikff12@gmail.com>
2022-12-30 13:30:05 +0200

    navbar and the register/login page

--- client/client_side/src/App.js
+++ client/client_side/src/App.js
@@ -1,6 +1,7 @@
 import AppRouter from "./components/AppRouter";
 import {BrowserRouter} from 'react-router-dom';
 import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
     <BrowserRouter>
--- client/client_side/src/components/Navbar.js
+++ client/client_side/src/components/Navbar.js
@@ -9,22 +9,30 @@ import Navbar from 'react-bootstrap/Navbar';
 import {NavLink} from "react-router-dom";
 import {MAIN_ROUTE} from "../utils/consts";
 import s from "../modules/styles.module.css"
-const NavBar = () => {
+import {Button} from "react-bootstrap";
+import {observer} from "mobx-react-lite";
+
+// set observer to identify the state
+const NavBar = observer( () => {
     const {user} = useContext(Context)
     return (
-        <Navbar className={s.navigation} expand="lg">
-            <Container fluid >
-                <NavLink  to={MAIN_ROUTE} className={s.logo}>SPORT_FINDER</NavLink>
 
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                    </Nav>
+        <Navbar className={s.navigation}>
+            <Container>
+                <NavLink className={s.logo} to ={MAIN_ROUTE}>SPORT_FINDER</NavLink>
+                {user.isAuth ? <Nav className="ml-auto" style={{maxHeight: '100px'}} navbarScroll>
+                                     <Button variant={"outline-dark"}>Профіль</Button>
+                                    <Button variant={"outline-dark"} className={s.button}>Вийти</Button>
+                                 </Nav>
+                                 : <Nav className="ml-auto"  style={{maxHeight: '100px'}} navbarScroll>
+                                    <Button variant={"outline-dark"}  onClick={() => user.setIsAuth(true)}>Авторизація</Button>
+                                 </Nav>
+                }
             </Container>
         </Navbar>
+
+
     );
-};
+})
 
 export default NavBar;
