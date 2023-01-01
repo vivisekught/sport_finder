b36f4c308e3135fb27db9d7a8471fd21ba3d242a
artisoul0 <yur4ikff12@gmail.com>
2023-01-01 15:13:45 +0200

    added axios to have a communication between frontend and backend

--- client/client_side/src/pages/Auth.js
+++ client/client_side/src/pages/Auth.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Container, Form, Card, Button} from 'react-bootstrap'
 import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
 import {NavLink, useLocation} from "react-router-dom";
+import {login, registration} from "../http/userAPI";
 const Auth = () => {
     const location = useLocation()
     const isLogin = location.pathname === LOGIN_ROUTE
-    console.log(location)
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const click = async () => {
+        if(isLogin){
+            const response = await login()
+
+        }
+        else{
+            const response = await registration(email, password)
+            console.log(response)
+        }
+
+
+    }
     return (
         <Container className="d-flex justify-content-center align-items-center" style={{height: window.innerHeight -54}}>
             <Card style={{width: 600}} className="p-5">
                 <h2 className="m-auto">{ isLogin ? 'Авторизація' : 'Реєстрація'} </h2>
                 <Form className="d-flex flex-column">
                     { isLogin ?
-                        <div><Form.Control type="email" className="mt-3" placeholder="Введіть ваш email" />
-                        <Form.Control type="password" className="mt-3" placeholder="Введіть ваш пароль" />
+                        <div>
+                            <Form.Control type="email" className="mt-3" placeholder="Введіть ваш email"
+                            value={email} onChange={e => setEmail(e.target.value)}
+                            />
+
+                        <Form.Control type="password" className="mt-3" placeholder="Введіть ваш пароль"
+                        value={password} onChange={e => setPassword(e.target.value)} typeof={password}
+                        />
                         </div> :
                         <div><Form.Control type="email" className="mt-3" placeholder="Введіть ваш email" />
                             <Form.Control type="password" className="mt-3" placeholder="Введіть ваш пароль" />
-                            <Form.Control type="password" className="mt-3" placeholder="Повторіть ваш пароль" />
                         </div>
                     }
                     <div className="d-flex justify-content-between mt-3">
@@ -27,7 +48,7 @@ const Auth = () => {
                             <div>
                             Є акаунт? <NavLink to = {LOGIN_ROUTE}>Увійти в акаунт</NavLink>
                             </div>}
-                        <Button variant={"outline-success"}>{isLogin ? "Увійти" : "Зареєструватись"}</Button>
+                        <Button variant={"outline-success"} onClick={click}>{isLogin ? "Увійти" : "Зареєструватись"}</Button>
                     </div>
 
                 </Form>
