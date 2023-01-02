a4bc51a6a035880a6f373312aa8da991da3ae2f4
vivisekught <nikitamaliy09@gmail.com>
2023-01-02 20:20:41 +0200

    create checkRole middleware

---
+++ server/middleware/checkRoleMiddleware.js
@@ -0,0 +1,29 @@
+const errors = require("../utils/Constants")
+const jwt = require('jsonwebtoken')
+
+module.exports = function(role){
+    return function (req, res, next) {
+        if (req.method === "OPTIONS") {
+            next()
+        } // because we are interested in post, get, delete
+
+        try {
+            // we need get token by first index, because 0 index is token type
+            const token = req.headers.authorization.split(' ')[1]
+            if (!token) {
+                return res.status(errors.unauthorized).json({message: "Unauthorized user"})
+            }
+            const decoded = jwt.verify(token, process.env.SECRET_KEY) // check token validation
+
+            if(decoded.role !== role){
+                return res.status(errors.forbidden).json({message: "No access"})
+            }
+
+            req.user = decoded
+            next()
+
+        } catch (e) {
+            res.status(errors.unauthorized).json({message: "Unauthorized user"})
+        }
+    };
+};
--- server/routers/trainingRouter.js
+++ server/routers/trainingRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const trainingController = require('../controllers/trainingController')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
 
-router.post('/', trainingController.create)
+router.post('/', checkRole('COACH'),trainingController.create)
 router.get('/', trainingController.getAll)
 router.get('/:id', trainingController.getOne)
 
