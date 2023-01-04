ca55b3f86920f69451a02e7cf0397b4b3e9fb5c3
artisoul0 <yur4ikff12@gmail.com>
2023-01-04 23:07:12 +0200

    finished questionary

--- client/client_side/src/pages/Questionary_second.js
+++ client/client_side/src/pages/Questionary_second.js
@@ -1,10 +1,10 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useReducer, useState} from 'react';
 import {Context} from "../index";
-import {Button, Col, Container, Form, Row} from "react-bootstrap";
+import {Button, Col, Container, Form, ListGroup, ListGroupItem, Row} from "react-bootstrap";
 import s from "../modules/styles.module.css";
-
 const QuestionarySecond = () => {
     const {user} = useContext(Context)
+    const [ignored, forceUpdate] = useReducer(x => x + 1, 0) //rerender the component
     const updateUserData = () =>{
          user.setUserPortfolioSecondPage(age, gender, weight, height, PhoneNumber, FavouriteSport)
         console.log(user.getUserPortfolio())
@@ -16,6 +16,8 @@ const QuestionarySecond = () => {
     const [height, setHeight] = useState("")
     const [PhoneNumber, setPhoneNumber] = useState("")
     const [FavouriteSport, setFavouriteSport] = useState("")
+
+    const {training} = useContext(Context)
     return (
         <Container>
 
@@ -26,12 +28,29 @@ const QuestionarySecond = () => {
                 <Col md={12} className="d-flex justify-content-center mt-5"><div className={s.input_size}><p>Зріст</p><Form.Control placeholder="Введіть ваш зріст" value={height} onChange={e => setHeight(e.target.value)}></Form.Control></div></Col>
                 <Col md={12} className="d-flex justify-content-center mt-5"><div className={s.input_size}><p>Вага</p><Form.Control placeholder="Введіть вашу вагу" value={weight} onChange={e => setWeight(e.target.value)}></Form.Control></div></Col>
                 <Col md={12} className="d-flex justify-content-center mt-5"><div className={s.input_size}><p>Номер телефону</p><Form.Control placeholder="Введіть ваш номер телефону" value={PhoneNumber} onChange={e => setPhoneNumber(e.target.value)}></Form.Control></div></Col>
-                <Col md={12} className="d-flex justify-content-center mt-5"><div className={s.input_size}><p>Улюблений вид спорту</p><Form.Control placeholder="Введіть ваш улюблений вид спорту" value={FavouriteSport} onChange={e => setPhoneNumber(e.target.value)}></Form.Control></div></Col>
+                <Col md={12} className="d-flex justify-content-center mt-5"><div className={s.input_size}><p>Улюблений вид спорту</p>
+                            <ListGroup>
+                                {training.kinds.map(kind =>
+                                    <ListGroupItem key={kind.id} onClick={ () => {
+                                        training.setSelectedKind(kind)
+                                        setFavouriteSport(training.selectedKinds.name)
+                                        forceUpdate()
+                                    }}
+                                                   active={kind.id === training.selectedKinds.id}
+                                                   style={{cursor: "pointer"}}
+                                    >
+                                        {kind.name}
+                                    </ListGroupItem>
+                                )}
+                            </ListGroup>
+
+
+                </div></Col>
                 <Col md={12} className="d-flex justify-content-center mt-5"><Button variant={"outline-primary" } className={s.input_size}
                                                                                     onClick={() =>{
                                                                                         updateUserData()
-                                                                                    }
-                                                                                    }>Продовжити</Button></Col>
+                                                                                        console.log(user.getUserPortfolio())
+                                                                                    }}>Продовжити</Button></Col>
             </Row>
 
         </Container>
